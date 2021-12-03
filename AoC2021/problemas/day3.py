6ce0c9351d32aa3a46b7c3925e861e86d279575f
Alejandro Llobell Alfaro <alloalf@upv.edu.es>
2021-12-03 16:50:52 +0100

    day3 part1

--- AoC2021/problemas/day3.py
+++ AoC2021/problemas/day3.py
@@ -1,10 +1,29 @@
+from collections import Counter
+
+
+def parte1(data):
+    num = [Counter(i) for i in zip(*data)]
+    res = [1 if  i.get('1') > i.get('0') else 0 for i in num]
+    gamma = 0; epsilon = 0
+    p = len(data[0]) - 1
+    for i in num:
+        if i.get('1') > i.get('0'):
+            gamma += pow(2, p)
+        else:
+            epsilon += pow(2, p)
+        p -= 1
+    return gamma * epsilon
+
+
+def parte2(data):
+    return 0
+
 def main():
-    address = "./data/day2"
+    address = "./data/day3"
     file = open(address, "r")
     data = file.read().split("\n")
     file.close()
     print(data)
-    print("hola")
-    #problems = {1: parte1, 2: parte1}
+    problems = {1: parte1, 2: parte2}
     prob = int(input("¿Problema 1 o 2?\n"))
-    #print("El resultado es: ", problems[prob](data))
+    print("El resultado es: ", problems[prob](data))
