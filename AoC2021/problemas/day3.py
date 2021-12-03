47ef27872e7284dd1b5fc0ce4f1829bf7bcb40ad
Alejandro Llobell Alfaro <alloalf@upv.edu.es>
2021-12-03 23:52:14 +0100

    no sale la parte2

--- AoC2021/problemas/day3.py
+++ AoC2021/problemas/day3.py
@@ -6,21 +6,29 @@ def reverse(n):
 
 
 def parte1(data):
-    num = [count(i) for i in zip(*data)]
+    num = [Counter(i) for i in zip(*data)]
     res = ['1' if i.get('1') > i.get('0') else '0' for i in num]
     gamma = int(''.join(res), 2)
     epsilon = int(reverse(res), 2)
     return gamma * epsilon
 
+
 def parte2(data):
-    return 0
+    num = ['']*len(data[0])
+    for i in data:
+        for j in range(len(data[0])):
+            num[j] += i[j]
+    c = [Counter(i) for i in num]
+    aux = ['1' if x['1'] >= x['0'] else '0' for x in c]
+    co = ''.join(['0' if x['0'] <= x['1'] else '1' for x in c])
+
+
 
 def main():
     address = "./data/day3"
     file = open(address, "r")
     data = file.read().split("\n")
     file.close()
-    print(data)
     problems = {1: parte1, 2: parte2}
     prob = int(input("¿Problema 1 o 2?\n"))
     print("El resultado es: ", problems[prob](data))
