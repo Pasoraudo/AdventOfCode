d794a69df1a2bd76182362a6578eae0ca56fc3b2
Alejandro Llobell Alfaro <alloalf@upv.edu.es>
2021-12-03 20:17:16 +0100

    day3 parte 1 fixed

--- AoC2021/problemas/day3.py
+++ AoC2021/problemas/day3.py
@@ -1,15 +1,14 @@
-from collections import Counter as count
+from collections import Counter
 
 
 def reverse(n):
-    return ''.join(['1' if i == '1' else '0' for i in n])
+    return ''.join(['0' if i == '1' else '1' for i in n])
 
 
 def parte1(data):
     num = [count(i) for i in zip(*data)]
     res = ['1' if i.get('1') > i.get('0') else '0' for i in num]
     gamma = int(''.join(res), 2)
-    print(res)
     epsilon = int(reverse(res), 2)
     return gamma * epsilon
 
