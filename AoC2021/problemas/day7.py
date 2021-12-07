940de4b0166676b7fd776f142e12bac472dbe260
Alejandro Llobell Alfaro <alloalf@upv.edu.es>
2021-12-07 08:33:40 +0100

    Dia 7

--- AoC2021/problemas/day7.py
+++ AoC2021/problemas/day7.py
@@ -1,12 +1,19 @@
+import statistics as s
 
 
-def problema(input, parte):
-    return 0
+def parte1(crabs):
+    posH = int(s.median(crabs))
+    return sum([abs(i - posH) for i in crabs])
+
+
+def parte2(crabs):
+    posH = int(s.mean(crabs))
+    return sum([i for i in [sum(range(1, abs(crab - posH)  + 1)) for crab in crabs]])
 
 
 def main():
     with open("./data/day7", 'r') as f:
-        data = []
+        data = [int (i) for i in f.read().split(',')]
     print('Los resultados del dia 7')
-    print("Parte 1:", problema(data.copy(), 1))
-    print("Parte 2:", problema(data.copy(), 2))
+    print("Parte 1:", parte1(data.copy()))
+    print("Parte 2:", parte2(data.copy()))
