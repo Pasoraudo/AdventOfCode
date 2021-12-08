fbd16236adc55035f9333f113240f7c529a9697b
Alejandro Llobell Alfaro <alloalf@upv.edu.es>
2021-12-08 14:00:52 +0100

    preparados los dias 9-12

--- AoC2021/__init__.py
+++ AoC2021/__init__.py
@@ -1,4 +1,4 @@
-from AoC2021.problemas import day1, day2, day3, day4, day5, day6, day7, day8
+from AoC2021.problemas import day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12
 def allDays(days):
     print('Ejecutando todos los dias...')
     for day in days.values():
@@ -8,7 +8,8 @@ def allDays(days):
 
 if __name__ == '__main__':
     day = input("Escoja el dia: ")
-    days = {'1': day1, '2': day2, '3': day3, '4': day4, '5': day5, '6': day6, '7': day7, '8': day8}
+    days = {'1': day1, '2': day2, '3': day3, '4': day4, '5': day5, '6': day6, '7': day7, '8': day8, '9': day9,
+            '10': day10, '11': day11, '12': day12}
     #try:
     if day == 'all':
         allDays(days)
---
+++ AoC2021/problemas/day10.py
@@ -0,0 +1,13 @@
+def parte1(input):
+    return 0
+
+def parte2(input):
+    return 0
+
+
+def main():
+    with open("./data/day10", 'r') as f:
+        data = [int (i) for i in f.read().split(',')]
+    print('Los resultados del dia 10')
+    print("Parte 1:", parte1(data))
+    print("Parte 2:", parte2(data))
