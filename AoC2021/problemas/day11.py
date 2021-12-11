3acd3c628cd6802e1f0500043f0c39baddcc5e6b
Alejandro Llobell Alfaro <alloalf@upv.edu.es>
2021-12-11 12:51:28 +0100

    day11 completo

--- AoC2021/problemas/day11.py
+++ AoC2021/problemas/day11.py
@@ -1,13 +1,59 @@
-def parte1(input):
-    return 0
+import numpy as np
 
-def parte2(input):
-    return 0
+
+def parte1(m, steps):
+    explosions = 0
+    rows = m.shape[0]
+    cols = m.shape[1]
+    for i in range(steps):
+        alreadyFlash = []
+        for x in range(rows):
+            for y in range(cols):
+                if (x, y) not in alreadyFlash:
+                    m[x, y] += 1
+                if m[x, y] > 9 and (x, y) not in alreadyFlash:
+                    alreadyFlash.append((x, y))
+                    m[x, y] = 0
+                    explosions += 1 + makeExplosion(m, x, y, alreadyFlash)
+    return explosions
+
+def makeExplosion(m, x, y, alreadyFlash):
+    exlosions = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            xProv = x + i
+            yProv = y + j
+            if 0 <= xProv < m.shape[0] and 0 <= yProv < m.shape[1]:
+                if (xProv, yProv) not in alreadyFlash:
+                    m[xProv, yProv] += 1
+                if m[xProv, yProv] > 9:
+                    alreadyFlash.append((xProv, yProv))
+                    m[xProv, yProv] = 0
+                    exlosions += 1 + makeExplosion(m, xProv, yProv, alreadyFlash)
+    return exlosions
+
+def parte2(m):
+    steps = 0
+    rows = m.shape[0]
+    cols = m.shape[1]
+    while m.any():
+        steps += 1
+        alreadyFlash = []
+        for x in range(rows):
+            for y in range(cols):
+                if (x, y) not in alreadyFlash:
+                    m[x, y] += 1
+                if m[x, y] > 9 and (x, y) not in alreadyFlash:
+                    alreadyFlash.append((x, y))
+                    m[x, y] = 0
+                    makeExplosion(m, x, y, alreadyFlash)
+    return steps
+
 
 
 def main():
     with open("./data/day11", 'r') as f:
-        data = [int (i) for i in f.read().split(',')]
+        data = np.matrix([[int(j) for j in i[:-1]] for i in f.readlines()])
     print('Los resultados del dia 11')
-    print("Parte 1:", parte1(data))
+    print("Parte 1:", parte1(data.copy(), 100))
     print("Parte 2:", parte2(data))
